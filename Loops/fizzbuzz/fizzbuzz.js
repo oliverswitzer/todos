fb96aef8ac70b9dfb84c2668445e2aa73a003011
oliverswitzer <oliverswitzer@gmail.com>
2013-11-14 20:08:47 -0500

    bug fix: iterator was being printed regardless of whether it was divisible by 5 or 3

--- Loops/fizzbuzz/fizzbuzz.js
+++ Loops/fizzbuzz/fizzbuzz.js
@@ -11,5 +11,4 @@ for(var i = 1; i < 101; i++) {
 	else  {
 		console.log(i);
 	}
-
 }
--- Loops/fizzbuzz/fizzbuzz.rb
+++ Loops/fizzbuzz/fizzbuzz.rb
@@ -1,9 +1,11 @@
 1.upto(100) do |i|
-  puts i 
-  next if i % 3 == 0
+  if i % 3 == 0
     puts "fizz"
-  next if i % 5 == 0
+  elsif i % 5 == 0
     puts "buzz"
-  next if i % 3 == 0 && i % 5 == 0
+  elsif i % 3 == 0 && i % 5 == 0
     puts "fizzbuzz"
+  else
+    puts i
+  end
 end
